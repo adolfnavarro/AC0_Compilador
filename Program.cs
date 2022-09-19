e4845cd9a2c5a089af1e2a6b69c58a7af4483d94
adolfnavarro <adolfnavarro@gmail.com>
2022-09-19 18:51:52 +0200

    Cuarto intento

--- Program.cs
+++ Program.cs
@@ -5,13 +5,14 @@ internal class Program
     {
         int number1, number2, total;
 
-        Console.WriteLine("Dona'm el primer valor: ");
+        Console.WriteLine("Dona'm el primer valor,: ");
         number1 = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Dona'm el segon valor: ");
         number2 = Convert.ToInt32(Console.ReadLine());
         total = number1 + number2;
         Console.WriteLine("la suma correcta és: " + total);
         Console.ReadLine();
+        //Made by adolf 
     }
 }
 
